c4e5190f5d90d60f418dc466dc286d47ab2f2420
7assan-5alloof <hassankhalloof20#gmail.com>
2020-12-21 12:16:48 +0400

    Write part of the program

--- main.cpp
+++ main.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+void minMax(int, int)
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Press Ctrl-C to stop" << endl;
+
+    int num;
+    while (true)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            cout << "Enter a number: "
+            cin >> num;
+        }
+        minMax(int &m, int &M);
+    }
+
     return 0;
+}
+
+void minMax(int m, int M)
+{
+
+    if (i == 1)
+    {
+        M = num, m = num;
+        continue;
+    }
 }
